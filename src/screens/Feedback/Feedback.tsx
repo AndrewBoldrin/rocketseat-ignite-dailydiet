53c2c991dc588a278ca9c09a9fe3e883174f645b
andrew boldrin <andrewboldrin@hotmail.com>
2023-02-18 15:30:20 -0300

    feat(Feedback): add Button to feedback screen

--- src/screens/Feedback/Feedback.tsx
+++ src/screens/Feedback/Feedback.tsx
@@ -2,6 +2,7 @@ import { FeedbackMessage } from "components/FeedbackMessage";
 import { Container, FeedbackImage } from "./styles";
 import positive_feedback from "assets/positive_feedback.png";
 import negative_feedback from "assets/negative_feedback.png";
+import { Button } from "components/Button";
 
 export function Feedback() {
   const inDiet = false;
@@ -10,6 +11,11 @@ export function Feedback() {
     <Container>
       <FeedbackMessage inDiet={inDiet} />
       <FeedbackImage source={inDiet ? positive_feedback : negative_feedback} />
+      <Button
+        type="NAVEGATION"
+        text="Ir para a página inicial"
+        style={{ marginTop: 32 }}
+      ></Button>
     </Container>
   );
 }
