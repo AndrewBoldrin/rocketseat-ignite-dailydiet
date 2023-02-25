7ae5b058b86e673bb432afd66a9c4c8096d4499b
andrew boldrin <andrewboldrin@hotmail.com>
2023-02-24 21:36:38 -0300

    feat(NavigationTypes): add type for navigation routes

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,17 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined;
+      stats: undefined;
+      form: {
+        MealId?: string;
+      };
+      meal: {
+        id: string;
+      };
+      feedback: {
+        inDiet: boolean;
+      };
+    }
+  }
+}
